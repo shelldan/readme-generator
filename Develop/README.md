a17d7d7eb035d440cd24404ce939475083d1f529
shelldan <shelldanlee@gmail.com>
2022-09-28 17:35:52 -0500

    update

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+# costco
+
+## Description
+saving
+
+## Table of Contents
+* [Installation](doc:Installation)
+* Usage
+* Contribution
+* Test
+* License
+* Question
+
+
+## Installation
+apple
+
+## Usage
+money
+
+## Contribution
+no cont
+
+## Test
+no test
+
+## License
+* MIT
+
+## Question
+* Click [here](https://github.com/shelldan) to go to my Github Repositories
+* feel free to reach me at shelldanlee@gmail.com if you have additional questions
+
---
+++ Develop/index.js
@@ -0,0 +1,122 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const { title } = require('process')
+
+const generateREADME = ({ 
+    title, 
+    description,
+    installation,
+    usage,
+    contribution,
+    test, 
+    license,
+    github,
+    email,
+}) =>
+`# ${title}
+
+## Description
+${description}
+
+## Table of Contents
+* [Installation](doc:Installation)
+* Usage
+* Contribution
+* Test
+* License
+* Question
+
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contribution
+${contribution}
+
+## Test
+${test}
+
+## License
+* ${license}
+
+## Question
+* Click [here](https://github.com/${github}) to go to my Github Repositories
+* feel free to reach me at ${email} if you have additional questions
+
+`
+
+
+
+
+// TODO: Create an array of questions for user input
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your APP?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the APP for?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How to install the APP?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How to use the APP?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How to make contribution for the APP?'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How to test the APP?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'what is the license?',
+        choices: ['MIT','GPLv3','GPL']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'what is your github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+])
+.then((answers)=>{
+    const readmeContent = generateREADME(answers);
+
+    fs.writeFile('README.md',readmeContent,(err)=> err
+    ? console.log(err)
+    : console.log('Successfully created README.md'))
+})
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
